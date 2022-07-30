da5b9add35dbfcb90828ee1df899356a8dafaaf9
laoycz <1023716557@qq.com>
2022-07-30 17:28:32 +0800

    添加计数器

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-    <div @click="newGame()" class="face" :class="game.status"></div>
+    <div style="display: flex">
+      <div style="display: flex">
+        <div class="bg digit" style="background-position: 0 0"></div>
+        <div
+          class="bg digit"
+          :style="`background-position: ${minesCount[0] * -19.5}px 0`"
+        ></div>
+        <div
+          class="bg digit"
+          :style="`background-position: ${minesCount[1] * -19.5}px 0`"
+        ></div>
+      </div>
+      <div @click="newGame()" class="face" :class="game.status"></div>
+      <div style="display: flex">
+        <div
+          class="bg digit"
+          :style="`background-position:${splitedTime[0] * -19.5}px 0`"
+        ></div>
+        <div
+          class="bg digit"
+          :style="`background-position:${splitedTime[1] * -19.5}px 0`"
+        ></div>
+        <div
+          class="bg digit"
+          :style="`background-position:${splitedTime[2] * -19.5}px 0`"
+        ></div>
+      </div>
+    </div>
     <div @contextmenu.prevent class="zone">
       <div v-for="(row, i) in zone" :key="i" style="display: contents">
         <div
           v-for="(cell, j) in row"
           :key="j"
-          class="cell"
+          class="bg"
           :style="getCellStyle(cell)"
           @click="open([j, i])"
           @click.right="flag([j, i])"
@@ -23,6 +50,7 @@ import Matrix from "./Matrix";
 import { distribute, fill, fillStatus } from "./Zone";
 import type { status } from "./Zone";
 import Game from "./Game";
+import { count } from "console";
 
 // const matrix = new Matrix(distribute(99, 30, 16));
 // const zone = fillStatus(fill(matrix));
@@ -30,7 +58,7 @@ import Game from "./Game";
 export default defineComponent({
   data() {
     const game = new Game();
-    return { zone: game.zone.matrix, game };
+    return { zone: game.zone.matrix, game, time: 0 };
   },
   methods: {
     getCellStyle([value, status]: [number, status]): StyleValue {
@@ -68,6 +96,7 @@ export default defineComponent({
     newGame() {
       this.game = new Game();
       this.zone = this.game.zone.matrix;
+      this.time = 0;
     },
 
     open([x, y]: [number, number]) {
@@ -77,6 +106,27 @@ export default defineComponent({
     flag([x, y]: [number, number]) {
       this.game.flag([x, y]);
       this.zone = this.game.zone.matrix;
+    },
+    tick() {
+      if (this.game.status == "play") {
+        this.time = this.time + 1;
+      }
+    },
+  },
+  mounted() {
+    setInterval(this.tick, 1000);
+  },
+  computed: {
+    minesCount() {
+      const count = 99 - this.zone.flat().filter((a) => a[1] == "flag").length;
+      return [Math.floor(count / 10), count % 10];
+    },
+    splitedTime() {
+      return [
+        Math.floor(this.time / 100),
+        Math.floor(this.time / 10) % 10,
+        this.time % 10,
+      ];
     },
   },
 });
@@ -89,8 +139,14 @@ export default defineComponent({
   grid-auto-rows: 24px;
   justify-content: start;
 }
-.cell {
-  background-image: url(assets/sprite150.gif);
+
+.bg {
+  background-image: url(./assets/sprite150.gif);
+}
+
+.digit {
+  width: 20px;
+  height: 35px;
 }
 
 .face {
